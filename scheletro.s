431398352794df1eb380d1f7e7953de19e19341d
Max-Gorenca06 <153073404+Max-Gorenca06@users.noreply.github.com>
2026-03-03 15:03:43 +0100

    Add files via upload

---
+++ scheletro.s
@@ -0,0 +1,20 @@
+.globl _start #main
+
+.text # qua ci va codice
+
+_start: #qua ci va il nostro programma
+    
+exit:
+    addi x17,x0,10 # il numero 10=exit
+    ecall #chiamo il sistema operativo
+    
+
+    
+    
+    
+    
+    
+
+
+    
+    
